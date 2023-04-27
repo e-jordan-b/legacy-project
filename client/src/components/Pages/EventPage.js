91fa8f19914ed8cd4a06ff213a77fccd33789efc
Èric Jordan Bernardo <ericjordan1998@gmail.com>
2023-04-27 12:39:44 +0200

    Changed MongoURL and Token for Map

--- client/src/components/Pages/EventPage.js
+++ client/src/components/Pages/EventPage.js
@@ -53,7 +53,7 @@ useEffect(() => {
         <MapContainer className="event-page-map-container" center={[event.coordinates[0], event.coordinates[1]]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='<a href=\"https://www.jawg.io\" target=\"_blank\">&copy; Jawg</a> - <a href=\"https://www.openstreetmap.org\" target=\"_blank\">&copy; OpenStreetMap</a>&nbsp;contributors'
-          url="https://tile.jawg.io/jawg-sunny/{z}/{x}/{y}{r}.png?access-token=gLEFUdwGIyJxOzqWgXnDyQdBUquHAVUDvqJFUliKpH3e5FQ68AZTwUphVyo81Tmn"
+          url="https://tile.jawg.io/jawg-sunny/{z}/{x}/{y}{r}.png?access-token=Lxat7gsyaHnDL4HKxkcLO3VrDvJ54wKHK6PimdK6JwjzBNWNDL53V9ZPyTdYmf8U"
         />
           <Marker
           position={[event.coordinates[0],event.coordinates[1]]}
