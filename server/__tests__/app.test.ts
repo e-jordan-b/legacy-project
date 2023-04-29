f22eabc5abaef5be621621303e248de567ea2f58
Èric Jordan Bernardo <ericjordan1998@gmail.com>
2023-04-29 17:41:30 +0200

    not working

--- server/__tests__/app.test.ts
+++ server/__tests__/app.test.ts
@@ -1,34 +1,41 @@
-// const supertest = require('supertest')
-// const { app, server} = require('../index')
 import supertest from 'supertest'
 import { app, server } from '../index'
-import { connectDB, disconnectDB } from '../db'
+import mongoose from 'mongoose'
+// import { connectDB, disconnectDB, mongod } from '../db'
+import UserModel from '../models/user_model'
 const request = supertest(app)
-// const { connectDB, disconnectDB } = require('../db');
+
+const url: any = process.env.MONGODB_URI
+
+beforeEach(async () => {
+  await mongoose.connect(url);
+});
+
+afterEach(async () => {
+  await mongoose.connection.close();
+  // server.close();
+});
 
 describe('API test', () => {
-  beforeAll(() => {
-    connectDB();
-  });
-
-  afterAll(() => {
-    disconnectDB();
-    server.close();
-  });
 
   describe("get /users", () => {
+
     describe("given the required information", () => {
-      // should save user to the database
-      test('should return correct status', async () => {
+      it('should return correct status', async () => {
         const response = await request.get('/users')
         expect(response.statusCode).toBe(201)
+      }),
+      it('should return json in the content type header', async () => {
+        const response = await request.get('/users')
+        expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
       })
-      // should respond with a json object containing the user
-
-      // should specify json in the content type header
-      })
+      // test('should return list of all users in database', async () => {
+      //   const { body } = await request.get('/users')
+      //   console.log(body)
+      //   expect(
     })
   })
+})
 
 // test("should respong with a 201 status code", async () => {
 //   const response = await supertest(app).post("/user").send({
--- server/db.ts
+++ server/db.ts
@@ -1,50 +1,50 @@
 import mongoose from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server'
-import { connect } from 'http2';
+// import { MongoMemoryServer } from 'mongodb-memory-server'
+// import { connect } from 'http2';
 
 const DB_PORT = process.env.DB_PORT || 27017;
-const DB_NAME = process.env.DB_NAME || 'imin';
+const DB_NAME = process.env.DB_NAME || 'legacy';
 
-// try {
-//   // mongoose.connect(`mongodb://localhost:${DB_PORT}/${DB_NAME}`);
-//   mongoose.connect(`mongodb://127.0.0.1/${DB_NAME}`);
-//   console.log(`🦆 Database (sessions) connected @ port ${DB_PORT}!`);
-// } catch (err) {
-//   console.log(`😞 Sorry, something went wrong! ${err}`);
-// }
-
-
-let mongod: any = null;
-
-export const connectDB = async () => {
-  try {
-    let dbUrl = 'mongodb://username:password@localhost:27017';
-    if (process.env.NODE_ENV === 'test') {
-      mongod = await MongoMemoryServer.create();
-      dbUrl = mongod.getUri();
-    }
-
-    const conn = await mongoose.connect(`mongodb://127.0.0.1/${DB_NAME}`);
-
-    console.log(`MongoDB connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.log(err);
-    process.exit(1);
-  }
-};
-
-export const disconnectDB = async () => {
-  try {
-    await mongoose.connection.close();
-    if (mongod) {
-      await mongod.stop();
-    }
-  } catch (err) {
-    console.log(err);
-    process.exit(1);
-  }
+try {
+  // mongoose.connect(`mongodb://localhost:${DB_PORT}/${DB_NAME}`);
+  mongoose.connect(`mongodb://127.0.0.1/${DB_NAME}`);
+  console.log(`🦆 Database (sessions) connected @ port ${DB_PORT}!`);
+} catch (err) {
+  console.log(`😞 Sorry, something went wrong! ${err}`);
 }
 
-connectDB()
+
+// export let mongod: any = null;
+
+// export const connectDB = async () => {
+//   try {
+//     let dbUrl = 'mongodb://username:password@localhost:27017';
+//     if (process.env.NODE_ENV === 'test') {
+//       mongod = await MongoMemoryServer.create();
+//       dbUrl = mongod.getUri();
+//     }
+
+//     const conn = await mongoose.connect(`mongodb://127.0.0.1/${DB_NAME}`);
+
+//     console.log(`MongoDB connected: ${conn.connection.host}`);
+//   } catch (err) {
+//     console.log(err);
+//     process.exit(1);
+//   }
+// };
+
+// export const disconnectDB = async () => {
+//   try {
+//     await mongoose.connection.close();
+//     if (mongod) {
+//       await mongod.stop();
+//     }
+//   } catch (err) {
+//     console.log(err);
+//     process.exit(1);
+//   }
+// }
+
+// connectDB()
 
 export default mongoose;
